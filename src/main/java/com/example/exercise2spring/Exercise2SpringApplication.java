7e0bc8d639f78dee027d4ad55e8641a52a91ffa1
ShadyWestHaze <zakatistovu@gmail.com>
2024-04-26 15:21:22 +0300

    setup

---
+++ src/main/java/com/example/exercise2spring/Exercise2SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.exercise2spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise2SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise2SpringApplication.class, args);
+    }
+
+}
